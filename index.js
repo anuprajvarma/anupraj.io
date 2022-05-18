70476f16ffc249ed196db1b3645b4101aafc81f2
anuprajvarma <anupraj1854@gmail.com>
2022-05-18 21:56:34 +0530

    code refactor

--- index.js
+++ index.js
@@ -1,70 +1,51 @@
+
 document.querySelector(".home").style.display="block";
 document.querySelector(".about").style.display="none";
 document.querySelector(".project").style.display="none";
 document.querySelector(".blog").style.display="none";
 document.querySelector(".blog-section").style.display="none";
-document.querySelectorAll("a")[0].style.color="blue";
+document.querySelector(".close-icon").style.display="none";
 
-
-document.querySelectorAll("a")[0].addEventListener("click",function(){
+document.querySelector(".home-menu").addEventListener("click",function(){
         document.querySelector(".home").style.display="block";
         document.querySelector(".about").style.display="none";
         document.querySelector(".project").style.display="none";
         document.querySelector(".blog").style.display="none";
         document.querySelector(".blog-section").style.display="none";
-
-        document.querySelectorAll("a")[0].style.color="blue";
-        document.querySelectorAll("a")[1].style.color="black";
-        document.querySelectorAll("a")[2].style.color="black";
-        document.querySelectorAll("a")[3].style.color="black";
 })
-document.querySelectorAll("a")[1].addEventListener("click",function(){
+document.querySelector(".about-menu").addEventListener("click",function(){
   document.querySelector(".home").style.display="none";
   document.querySelector(".about").style.display="block";
   document.querySelector(".project").style.display="none";
   document.querySelector(".blog").style.display="none";
   document.querySelector(".blog-section").style.display="none";
-  
-  document.querySelectorAll("a")[0].style.color="black";
-  document.querySelectorAll("a")[1].style.color="blue";
-  document.querySelectorAll("a")[2].style.color="black";
-  document.querySelectorAll("a")[3].style.color="black";
 })
-document.querySelectorAll("a")[2].addEventListener("click",function(){
+document.querySelector(".project-menu").addEventListener("click",function(){
   document.querySelector(".home").style.display="none";
   document.querySelector(".about").style.display="none";
   document.querySelector(".project").style.display="block";
   document.querySelector(".blog").style.display="none";
   document.querySelector(".blog-section").style.display="none";
-
-  document.querySelectorAll("a")[0].style.color="black";
-  document.querySelectorAll("a")[1].style.color="black";
-  document.querySelectorAll("a")[2].style.color="blue";
-  document.querySelectorAll("a")[3].style.color="black";
 })
-document.querySelectorAll("a")[3].addEventListener("click",function(){
+document.querySelector(".blog-menu").addEventListener("click",function(){
   document.querySelector(".home").style.display="none";
   document.querySelector(".about").style.display="none";
   document.querySelector(".project").style.display="none";
   document.querySelector(".blog").style.display="block";
   document.querySelector(".blog-section").style.display="none";
-
-  document.querySelectorAll("a")[0].style.color="black";
-  document.querySelectorAll("a")[1].style.color="black";
-  document.querySelectorAll("a")[2].style.color="black";
-  document.querySelectorAll("a")[3].style.color="blue";
 })
-
 
 document.querySelector(".blog-heading").addEventListener("click",function(){
   document.querySelector(".blog-section").style.display="block";
   document.querySelector(".blog").style.display="none";
-
-  document.querySelectorAll("a")[0].style.color="black";
-  document.querySelectorAll("a")[1].style.color="blue";
-  document.querySelectorAll("a")[2].style.color="black";
-  document.querySelectorAll("a")[3].style.color="black";
 })
 
-
+document.querySelector(".menu-icon").addEventListener("click",function(){
+  document.querySelector(".close-icon").style.display="block";
+  document.querySelector(".menu-icon").style.display="none";
+})
+document.querySelector(".close-icon").addEventListener("click",function(){
+  document.querySelector(".menu-icon").style.display="block";
+  document.querySelector(".close-icon").style.display="none";
+})
 
