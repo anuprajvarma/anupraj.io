3ba14192c073faba88e48e756648f9c6c271c687
anuprajvarma <anupraj1854@gmail.com>
2022-05-21 22:12:36 +0530

    add portfolio pic

--- index.js
+++ index.js
@@ -43,12 +43,10 @@ document.querySelector(".blog-menu").addEventListener("click",function(){
         blog.style.display="block";
         blog_section.style.display="none"; 
 })
-
 document.querySelector(".blog-heading").addEventListener("click",function(){
         document.querySelector(".blog-section").style.display="block";
         document.querySelector(".blog").style.display="none";
 })
-
 menuIcon.addEventListener("click",function(){
         document.querySelector(".close-icon").style.display="block";
         document.querySelector(".menu-icon").style.display="none";
